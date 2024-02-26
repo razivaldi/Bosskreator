d54a80b5306874c54b900ebea0116b072cd099cd
razivaldi <razivaldi15@gmail.com>
2024-02-27 07:59:25 +0800

    add hero section + navbar

--- bosskreator/src/pages/index.js
+++ bosskreator/src/pages/index.js
@@ -1,15 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Hero from "@/components/hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-<h1>HOMEPAGE</h1>
-     
+    <main>
+      <Hero/>
     </main>
   );
 }
